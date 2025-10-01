83c0d61767a33deda3a5b75f52a3a507bf8ce8f6
Derrick Nyongesa <99530585+DECTEN0@users.noreply.github.com>
2025-10-01 16:34:24 +0000

    Exception

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-import logger
+from src.logger import logging
 
 def error_message_detail(error, error_detail: sys):
     """
@@ -25,6 +25,3 @@ class CustomException(Exception):
 
     def __str__(self):
         return self.error_message
-
-
-
